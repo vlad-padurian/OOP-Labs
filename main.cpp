b441b3b1622709b3dac14d48ed6b7f0d644c989a
vlad-padurian <vladpadyrian2@gmail.com>
2026-04-13 19:07:45 +0300

    added regular player and admin interface

--- main.cpp
+++ main.cpp
@@ -1,37 +1,28 @@
-#include <iostream>
 #include <string>
-#include <utility>
 #include <vector>
+#include <memory>    
+#include <fstream>   
+#include <stdexcept> 
+
+using namespace std;
 
 class IDamageable {
 public:
-    virtual void takeDamage(int amount) = 0; 
-    virtual ~IDamageable() {} 
+    virtual void takeDamage(int amount) = 0;
+    virtual ~IDamageable() {}
 };
 
 class WorldEntity {
 protected:
     double x, y;
 public:
-    WorldEntity(double _x = 0, double _y = 0) : x(_x), y(_y) {
-        std::cout << "[WorldEntity] Створено в (" << x << ", " << y << ")" << std::endl;
-    }
-
-    void identify() { 
-        std::cout << "[Base] Я просто сутність світу." << std::endl; 
-    }
-
-    virtual void action() { 
-        std::cout << "[Base] Сутність світу бездіяльно стоїть." << std::endl; 
-    }
-
-    virtual ~WorldEntity() {
-        std::cout << "[WorldEntity] Видалено зі світу" << std::endl;
-    }
+    WorldEntity(double _x = 0, double _y = 0) : x(_x), y(_y) {}
+    virtual void action() { cout << "[Entity] Бездіяльно стоїть.\n"; }
+    virtual ~WorldEntity() {}
 };
 
+
 class DiamondSword {
-private: 
     int damage;
 public:
     DiamondSword(int dmg = 7) : damage(dmg) {}
@@ -39,112 +30,157 @@ public:
 };
 
 class IronArmor {
-private: 
     int defense;
 public:
     IronArmor(int def = 5) : defense(def) {}
     int getDefense() const { return defense; }
 };
 
+
 class Player : public WorldEntity, public IDamageable {
 protected:
-    std::string nickname;
+    string nickname;
     int health;
-    static int playerCount;
     DiamondSword sword;
     IronArmor armor;
 
 public:
-    Player(std::string name, int hp, double _x = 0, double _y = 0) 
-        : WorldEntity(_x, _y), nickname(name), health(hp), sword(7), armor(5) {
-        playerCount++;
-        std::cout << "[Spawn] " << nickname << " заспавнився!" << std::endl;
-    }
+    Player(string name, int hp, double _x = 0, double _y = 0) 
+        : WorldEntity(_x, _y), nickname(name), health(hp) {}
 
-    void identify() { 
-        std::cout << "[Derived] Я Гравець: " << nickname << std::endl; 
-    }
+    string getNickname() const { return nickname; }
+    int getHealth() const { return health; }
 
-    void action() override { 
-        std::cout << "[Player] " << nickname << " махає мечем на " << sword.getDamage() << " шкоди!" << std::endl; 
+    void action() override {
+        cout << "[Player] " << nickname << " махає мечем на " << sword.getDamage() << " шкоди!\n";
     }
 
     void takeDamage(int amount) override {
         int finalDamage = amount - armor.getDefense();
         if (finalDamage < 0) finalDamage = 0;
         health -= finalDamage;
-        std::cout << "[Hit] " << nickname << " отримав " << finalDamage << " шкоди. HP: " << health << std::endl;
+        cout << "[Hit] " << nickname << " отримав " << finalDamage << ". HP: " << health << "\n";
     }
 
-    void operator-() { 
-        health -= 10; 
-        std::cout << "[Effect] " << nickname << " отримав шкоду!" << std::endl; 
-    }
-
-    Player& operator+(int heal) { 
-        health += heal; 
-        return *this; 
-    }
-
-    friend std::ostream& operator<<(std::ostream& os, const Player& p) {
-        os << "Нік: " << p.nickname << " | HP: " << p.health;
+    friend ostream& operator<<(ostream& os, const Player& p) {
+        os << "Гравець: " << p.nickname << " | HP: " << p.health;
         return os;
     }
+};
 
-    static int getOnline() { return playerCount; }
 
-    ~Player() {
-        if (!nickname.empty()) {
-            playerCount--;
-            std::cout << "[Quit] " << nickname << " вийшов з гри!" << std::endl;
+class GameManager {
+private:
+    vector<unique_ptr<Player>> players; 
+    bool isAdmin = false;              
+    const string adminPassword = "admin228pass";
+    const string dataFile = "world_data.txt";
+    const string historyFile = "history.txt";
+
+    void logAction(string msg) {
+        ofstream log(historyFile, ios::app);
+        if (log.is_open()) {
+            log << "[HISTORY]: " << msg << "\n";
+            log.close();
+        }
+    }
+
+public:
+    GameManager() {
+        load(); 
+    }
+
+    void save() {
+        ofstream out(dataFile);
+        if (out.is_open()) {
+            for (size_t i = 0; i < players.size(); ++i) {
+                out << players[i]->getNickname() << " " << players[i]->getHealth() << "\n";
+            }
+            out.close();
+            logAction("Дані успішно збережено у файл.");
+        }
+    }
+
+    void load() {
+        try {
+            ifstream in(dataFile);
+            if (!in) throw runtime_error("Файл даних не знайдено .");
+            
+            players.clear();
+            string name;
+            int hp;
+            while (in >> name >> hp) {
+               
+                players.push_back(unique_ptr<Player>(new Player(name, hp)));
+            }
+            in.close();
+            cout << "[System] Дані завантажено.\n";
+        } catch (const exception& e) {
+            cout << "[System] " << e.what() << "\n";
+        }
+    }
+
+    void login() {
+        int mode;
+        cout << "--- ВХІД ---\n1. Користувач (Звичайний гравець)\n2. Адміністратор\nВибір: ";
+        cin >> mode;
+        if (mode == 2) {
+            string pass;
+            cout << "Введіть пароль: ";
+            cin >> pass;
+            if (pass == adminPassword) {
+                isAdmin = true;
+                cout << "Доступ Адміна дозволено!\n";
+                logAction("Адмін увійшов у систему.");
+            } else {
+                cout << "Пароль невірний! Ви увійшли як Звичайний гравець.\n";
+            }
+        }
+    }
+
+    void run() {
+        int choice = -1;
+        while (choice != 0) {
+            cout << "\n=== КОНСОЛЬНЕ МЕНЮ ===\n";
+            if (isAdmin) {
+                cout << "1. Додати гравця (Лише адмін)\n";
+                cout << "2. Очистити світ (Лише адмін)\n";
+            }
+            cout << "3. Переглянути список гравців\n";
+            cout << "0. Вихід\nВибір: ";
+            cin >> choice;
+
+            if (choice == 1 && isAdmin) {
+                string n; int h;
+                cout << "Нік: "; cin >> n;
+                cout << "HP: "; cin >> h;
+                players.push_back(unique_ptr<Player>(new Player(n, h)));
+                save(); 
+                logAction("Адмін додав гравця: " + n);
+            } 
+            else if (choice == 2 && isAdmin) {
+                players.clear();
+                save();
+                logAction("Адмін очистив список гравців.");
+                cout << "Світ очищено.\n";
+            }
+            else if (choice == 3) {
+                cout << "\n--- ГРАВЦІ В ОНЛАЙНІ ---\n";
+                if (players.empty()) cout << "Порожньо...\n";
+                for (size_t i = 0; i < players.size(); ++i) {
+                    cout << *players[i] << "\n";
+                }
+            }
         }
     }
 };
 
-int Player::playerCount = 0;
-
-class Admin final : public Player {
-public:
-    Admin(std::string name) : Player(name, 999) {}
-    
-    void action() override {
-        std::cout << "[Admin] " << nickname << " редагує світ (Creative Mode)!" << std::endl;
-    }
-};
-
-class WoodenBlock : public IDamageable {
-public:
-    void takeDamage(int amount) override {
-        std::cout << "[Block] Дерев'яний блок тріщить! Отримано " << amount << " шкоди." << std::endl;
-    }
-};
-
 int main() {
-    std::cout << "--- 1. Static Binding ---" << std::endl;
-    Player p1("Vlados", 50, 10, 20);
-    WorldEntity* staticPtr = &p1;
-    staticPtr->identify(); 
-    
-    std::cout << "\n--- 3. Dynamic Polymorphism (Pointer) ---" << std::endl;
-    WorldEntity* polyPtr = &p1;
-    polyPtr->action(); 
-    
-    std::cout << "\n--- 6. Dynamic Polymorphism (Reference) ---" << std::endl;
-    Admin god("Notch");
-    WorldEntity& entityRef = god;
-    entityRef.action(); 
-    
-    std::cout << "\n--- 8. Interface IDamageable ---" << std::endl;
-    WoodenBlock oakLog;
-    IDamageable* targets[2];
-    targets[0] = &p1;     
-    targets[1] = &oakLog; 
+    setlocale(LC_ALL, "Ukrainian");
 
-    for(int i = 0; i < 2; i++) {
-        targets[i]->takeDamage(15); 
-    }
+    GameManager game;
+    game.login();
+    game.run();
 
-    std::cout << "\n--- Destructors ---" << std::endl;
-    
     return 0;
 }
